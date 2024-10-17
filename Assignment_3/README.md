a3112e83b4523a8654d4c81bdc616e0f0a138e28
Allan Constanza <ajconstanzaaguirre@csuchico.edu>
2024-10-16 21:04:23 -0700

    Latest changes

--- Assignment_3/README.md
+++ Assignment_3/README.md
@@ -1,1 +1,6 @@
 # CS580_Fall24_Allan_Constanza
+We first import the libraries. Pandas is a tool used a lot for data analysis and matplotlib.pyplot is use for plotting graphs. 
+We then load the data using read_csv(). The independent and dependent variables are then extracted from the dataset.  
+We compute the mean, covariance, and variance using .mean(), data.cov(), independent_variable.var(). After we've calculated the slope and 
+intercept using the covariance and variance, we plot the data points and regression line. We use plt.scatter() for the scatter plot and
+plt.plot() for the regression line. Finally, we add the labels and plt.show() to display the plot. 
--- Assignment_3/linear_regression.py
+++ Assignment_3/linear_regression.py
@@ -1,40 +1,41 @@
-import pandas as pd #Powerful library for data analysis in Python, more convenient way to read and manipulate CSV data
-import matplotlib.pyplot as plt #plotting library in Python used for creating high-quality visualizations
+import pandas as pd #library for data analysis in Python, more convenient way to read and manipulate CSV data
+import matplotlib.pyplot as plt #plotting library in Python used for creating visualizations
 
 # our dataset has no headers
 data = pd.read_csv('linear_regression_data.csv', header=None)
 
 # Assigning column names b/c our file doesn't have headers
-#first column called X, second called Y
-data.columns = ['X', 'Y']
+data.columns = ['independent_variable', 'dependent_variable']
 
 
 #Our file doesnt have headers, manually assigning names 
-X = data['X']  # Independent variable
-Y = data['Y']  # Dependent variable
+independent_variable = data['independent_variable']  # Independent variable
+dependent_variable = data['dependent_variable']  # Dependent variable
 
 
-#getting mean of X and Y columns
-mean_x = X.mean() 
-mean_y = Y.mean()
+#getting mean of both columns columns
+mean_independent = independent_variable.mean() 
+mean_dependent = dependent_variable.mean()
 
-#getting mean of X and Y columns using data.cov() function
-covariance = data.cov().loc['X', 'Y']
-variance = X.var()
+#getting covariance of both columns using data.cov() function
+covariance = data.cov().loc['independent_variable', 'dependent_variable']
+variance = independent_variable.var()
 
 # Calculate slope (m) and intercept (c)
 m = covariance / variance
-c = mean_y - m * mean_x
+c = mean_dependent - m * mean_independent
 
 # Plot the data
-plt.scatter(X, Y, color = 'green',label='Data Points')
+plt.scatter(independent_variable, dependent_variable, color = 'green',label='Data Points')
 
 #regression line, y=mx+c
-Y_pred = m * X + c
-plt.plot(X, Y_pred, color='red', label='Regression Line')
+dependent_line = m * independent_variable + c
 
-plt.xlabel('X')  # X-axis label
-plt.ylabel('Y')  # Y-axis label
+
+plt.plot(independent_variable, dependent_line, color='red', label='Regression Line')
+
+plt.xlabel('Independent Variable')  # X-axis label
+plt.ylabel('Dependent Variable')  # Y-axis label
 plt.title('Linear Regression, Problem 1')
 plt.legend()
 plt.show()
